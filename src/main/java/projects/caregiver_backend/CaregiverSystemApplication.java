23ac605a27ed513e6f0c6cb2da4639a67983483b
Eddy-star223 <eddyeffanga@gmail.com>
2026-01-24 00:16:35 +0100

    build success commit

---
+++ src/main/java/projects/caregiver_backend/CaregiverSystemApplication.java
@@ -0,0 +1,13 @@
+package projects.caregiver_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregiverSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaregiverSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projects/caregiver_backend/CaregiverSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.caregiver_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregiverSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
