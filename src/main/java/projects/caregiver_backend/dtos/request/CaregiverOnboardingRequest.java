2866dcc3a02459236d5efb3b77a5d56d1f380ff9
Eddy-star223 <eddyeffanga@gmail.com>
2026-02-08 18:19:13 +0100

    modified caregiver onboarding request

--- src/main/java/projects/caregiver_backend/dtos/request/CaregiverOnboardingRequest.java
+++ src/main/java/projects/caregiver_backend/dtos/request/CaregiverOnboardingRequest.java
@@ -1,8 +1,10 @@
 package projects.caregiver_backend.dtos.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CaregiverOnboardingRequest {
 
     private String fullName;
