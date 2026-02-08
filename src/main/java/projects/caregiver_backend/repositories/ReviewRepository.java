2f488e6d9a41f5cde55a98197eb51d41af2072b7
Eddy-star223 <eddyeffanga@gmail.com>
2026-02-08 18:26:41 +0100

    modified review repo

--- src/main/java/projects/caregiver_backend/repositories/ReviewRepository.java
+++ src/main/java/projects/caregiver_backend/repositories/ReviewRepository.java
@@ -3,8 +3,8 @@ package projects.caregiver_backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import projects.caregiver_backend.model.Review;
-import projects.caregiver_backend.model.User;
 import projects.caregiver_backend.model.Caregiver;
+import projects.caregiver_backend.model.User;
 import projects.caregiver_backend.repositories.projections.CaregiverRatingView;
 
 import java.util.List;
